5f519892c7c37ef4f35f2cfaba15a61a4cbbba99
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2024-02-19 20:20:23 +0100

    initial setup for Vite app

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import 'tailwindcss/tailwind.css'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(<div>New app</div>)
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
+  theme: {
+    // colors: {
+    //   primary: 'oklch(66.28% 0.24 151.4 / <alpha-value>)',
+    //   secondary: 'oklch(66.35% 0.299 7.04 / <alpha-value>)',
+    //   tertiary:
+    //     'oklch(65.49% 0.1937998046114747 239.2963644488785 / <alpha-value>)'
+    // },
+    extend: {}
+  },
+  plugins: []
+}
