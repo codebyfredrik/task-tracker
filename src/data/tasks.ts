e47678312c9753f0ecb0b02ac021bb14a80daf5d
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2024-02-24 17:12:34 +0100

    Refactor code (#5)

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,18 +1,5 @@
 import { generateId } from '../utils'
-
-export type Priority = 'low' | 'medium' | 'high'
-
-export type Status = 'not started' | 'in progress' | 'completed' | 'all'
-
-export type Task = {
-  id: string
-  title: string
-  description: string
-  date?: string
-  status: Status
-  priority?: Priority
-  category?: Array<string>
-}
+import type { Task } from '../types'
 
 export const tasksWithoutId: Array<Omit<Task, 'id'>> = [
   {
